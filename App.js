fdcf570a022e389e5ad7842382c12b8318e27bae
uvin6667 <uvin6667@gmail.com>
2021-06-20 15:51:50 +0530

    Orientation

--- App.js
+++ App.js
@@ -2,17 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback, 
   TouchableOpacity, TouchableNativeFeedback, TouchableHighlight, Button, Alert, Dimensions } from 'react-native';
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
 
 export default function App() {
 
-  console.log(Dimensions.get('screen'));
+  const {landscape} = useDeviceOrientation();
   
   return (
     <SafeAreaView style={styles.container}>
      <View style={{
        backgroundColor: 'dodgerblue',
-       width: '50%',
-       height: 70
+       width: '100%',
+       height: landscape? '100%':'30%'
      }}>
      </View>
     </SafeAreaView>
