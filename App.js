9a70c868c09d5330d8ddec273526a24dde1080a8
uvin6667 <uvin6667@gmail.com>
2021-06-20 16:17:02 +0530

    Wrapping

--- App.js
+++ App.js
@@ -9,22 +9,35 @@ export default function App() {
      flex: 1,
      flexDirection: 'row',
      justifyContent: 'center',
-     alignItems: 'center'
+     alignItems: 'center',
+     alignContent: 'center',
+     flexWrap: 'wrap'
    }}>
      <View style={{
        backgroundColor: 'dodgerblue',
        width: 100,
        height: 300,
-       alignSelf: 'flex-start'
      }}/>
      <View style={{
        backgroundColor: 'gold',
        width: 100,
-       height: 200     }}/>
+       height: 100  
+     }}/>
      <View style={{
        backgroundColor: 'tomato',
        width: 100,
-       height: 100     }}/>
+       height: 100     
+    }}/>
+    <View style={{
+       backgroundColor: 'grey',
+       width: 100,
+       height: 100     
+    }}/>
+    <View style={{
+       backgroundColor: 'greenyellow',
+       width: 100,
+       height: 100     
+    }}/>
    </View>
   );
 }
