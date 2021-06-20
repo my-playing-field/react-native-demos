226c32a04e85d6e96e019f174ed2c90f4ca38f81
uvin6667 <uvin6667@gmail.com>
2021-06-20 17:56:47 +0530

    Add simple views

--- App.js
+++ App.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
 
   return (
-   <View style={{
-     backgroundColor: '#fff',
-     flex: 1,
-     flexDirection: 'row',
-     justifyContent: 'center',
-     alignItems: 'center'
-   }}>
-     <View style={{
-       backgroundColor: 'dodgerblue',
-       width: 100,
-       height: 100,
-     }}/>
-     <View style={{
-       backgroundColor: 'gold',
-       width: 100,
-       height: 100,
-       top:20,
-       left: 20, 
-       position: 'absolute'
-     }}/>
-     <View style={{
-       backgroundColor: 'tomato',
-       width: 100,
-       height: 100     
-    }}/>
-   </View>
+   <ViewImageScreen/>
   );
 }
 
