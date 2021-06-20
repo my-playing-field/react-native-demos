bbd3a548b0b6655f4490cd2bc1cbbeedc85aa41d
uvin6667 <uvin6667@gmail.com>
2021-06-20 16:05:40 +0530

    Flex direction

--- App.js
+++ App.js
@@ -7,19 +7,21 @@ export default function App() {
    <View style={{
      backgroundColor: '#fff',
      flex: 1,
+     flexDirection: 'row'
    }}>
      <View style={{
        backgroundColor: 'dodgerblue',
-       flex: 2,
+       width: 100,
+       height: 100
      }}/>
      <View style={{
        backgroundColor: 'gold',
-       flex: 1,
-     }}/>
+       width: 100,
+       height: 100     }}/>
      <View style={{
        backgroundColor: 'tomato',
-       flex: 1,
-     }}/>
+       width: 100,
+       height: 100     }}/>
    </View>
   );
 }
