b0509928ce4547adb824d719e96f4cf25b7be67a
uvin6667 <uvin6667@gmail.com>
2021-06-20 16:12:02 +0530

    Flex demos

--- App.js
+++ App.js
@@ -7,17 +7,20 @@ export default function App() {
    <View style={{
      backgroundColor: '#fff',
      flex: 1,
-     flexDirection: 'row'
+     flexDirection: 'row',
+     justifyContent: 'center',
+     alignItems: 'center'
    }}>
      <View style={{
        backgroundColor: 'dodgerblue',
        width: 100,
-       height: 100
+       height: 300,
+       alignSelf: 'flex-start'
      }}/>
      <View style={{
        backgroundColor: 'gold',
        width: 100,
-       height: 100     }}/>
+       height: 200     }}/>
      <View style={{
        backgroundColor: 'tomato',
        width: 100,
